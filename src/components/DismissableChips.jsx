5dbf0b72027aaa4b91977d1e80685a7133d43d8e
Shubham Mamgain <41307085+shubham23mamgain@users.noreply.github.com>
2024-04-13 02:10:20 +0530

    added filters

--- src/components/DismissableChips.jsx
+++ src/components/DismissableChips.jsx
@@ -1,4 +1,4 @@
-import {View, Alert} from 'react-native';
+import {View, Alert, FlatList, ScrollView} from 'react-native';
 import React, {useState} from 'react';
 import {Chip} from 'react-native-paper';
 
@@ -8,21 +8,25 @@ import {
 } from 'react-native-responsive-screen';
 
 const DismissableChips = () => {
-  const dataSource = ['Strap - Long', 'Colour', 'Size', 'Brand', 'Material'];
+  const dataSource = ['Strap - Long', 'Colour', 'Material', 'Size', 'Brand'];
   const [items, setItems] = useState(['Strap - Long']);
+  const newItem = '';
   const gray = 'rgba(156,163,175,1)';
   const blue = 'rgba(59, 130, 246, 1)';
   const lightblue = 'rgba(239,246,255,1)';
 
   return (
-    <View style={{flex: 1, flexDirection: 'row'}}>
+    <ScrollView horizontal={true} style={{flex: 1, flexDirection: 'row'}}>
       {dataSource.map((item, index) => {
+        console.log('Item', item);
+        console.log('Items', items);
         return (
           <View
             style={{
               marginTop: wp(1),
               marginRight: wp(1),
               flexWrap: 'wrap',
+              display: 'flex',
               justifyContent: 'start',
               alignItems: 'center',
             }}>
@@ -30,7 +34,10 @@ const DismissableChips = () => {
               key={index}
               mode="outlined"
               height={hp(4.5)}
-              onClose={() => {}}
+              onClose={() => {
+                if (items.includes(item))
+                  setItems(items.filter(a => a !== item));
+              }}
               width={wp(40)}
               textStyle={{
                 color: items.includes(item) ? blue : '#737373',
@@ -38,14 +45,20 @@ const DismissableChips = () => {
               }}
               style={{
                 backgroundColor: items.includes(item) ? lightblue : '#F4F4F4',
-              }} //display diff color BG
-              onPress={() => Alert.alert(item)}>
+              }}
+              onPress={() => {
+                // Alert.alert(item.name);
+                if (items.includes(item)) setItems([...items]);
+                else setItems([...items, item]);
+              }}>
               {item}
             </Chip>
           </View>
         );
       })}
-    </View>
+
+      {/* <FlatList data={dataSource} renderItem={{item}} /> */}
+    </ScrollView>
   );
 };
 
